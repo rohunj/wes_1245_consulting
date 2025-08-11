b818faefa1645f175fda4d30a28fa89f2a9de78f
Senthil Raja <mailtest151@gmail.com>
2025-08-11 22:34:51 +0100

    prevent duplicate page event

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   # Calendly OAuth routes
   get 'calendly/oauth/authorize', to: 'calendly#authorize'
   get 'calendly/oauth/callback', to: 'calendly#callback'
+  # Calendly Webhook Management routes
   get 'calendly/subscriptions', to: 'calendly#list_subscriptions'
   get 'calendly/create_subscription', to: 'calendly#create_manual_subscription'
   get 'calendly/access_token', to: 'calendly#get_access_token'
